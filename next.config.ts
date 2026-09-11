558f1ba9d82ef9f3aff6967f2a5796ce9d791933
sujith-lang <sujith7089@gmail.com>
2026-09-11 15:41:53 +0530

    images fixed

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "**",
       },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "plus.unsplash.com",
+        protocol: "http",
+        hostname: "**",
       }
     ],
   },
